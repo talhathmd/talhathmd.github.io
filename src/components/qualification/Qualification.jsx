3d8a4c106d0861b79d445b2ecddcd080f2709881
Talha Tahmid <talhathmd@gmail.com>
2024-09-16 16:56:03 -0500

    Added ACM info and resume update

--- src/components/qualification/Qualification.jsx
+++ src/components/qualification/Qualification.jsx
@@ -83,6 +83,23 @@ const Qualification = () => {
                 <div className={toggleState === 2 ? "qualification__content qualification__content-active" : "qualification__content"}>
                     
                     <div className="qualification__data">
+                        <div></div>
+
+                        <div>
+                            <span className="qualification__rounder"></span>
+                            <span className="qualification__line"></span>
+                        </div>
+                        <div>
+                            <h3 className="qualification__title">Officer</h3>
+                            <span className="qualification__subtitle">Association for Computing Machinery (ACM)</span>
+                            <div className="qualification__calendar">
+                                <i className="uil uil-calendar-alt"></i> Aug, 2024 - Present
+                            </div>
+                        </div>
+                        
+                    </div>
+
+                    <div className="qualification__data">
                        
                             <div>
                                 <h3 className="qualification__title">React Native Developer</h3>
